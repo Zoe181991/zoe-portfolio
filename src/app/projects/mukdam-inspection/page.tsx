de5e46897886b7f2ca6b3351f8c16bdcc1036f0f
Zoe181991 <zoebarkan@gmail.com>
2024-03-22 09:54:58 +0200

    textUpdated

--- src/app/projects/mukdam-inspection/page.tsx
+++ src/app/projects/mukdam-inspection/page.tsx
@@ -1,16 +1,48 @@
-import { Section, Footer, Navbar } from "ui";
+import {Section, Footer, Navbar, HeadLine, ImageGallery} from "ui";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
 
-      <main className="flex flex-col bg-body-bg-dots ">
-        <div className="bg-body-bg-shapes scroll-smooth h-screen	">
-          <Section id="aboutMe">mukdam-inspection</Section>
-          <Footer />
-        </div>
-      </main>
+    const imagesURLS = [{text:"1", url:"../../../../public/svg/mailIcon.svg"}, {text:"2", url:"../../../../public/svg/githubIcon.svg"}, {text:"3", url:"../../../../public/svg/likedinIcon.svg"}]
+
+    return (
+    <>
+        <Navbar />
+
+        <main className="flex flex-col bg-body-bg-dots ">
+            <div className="bg-body-bg-shapes scroll-smooth h-screen flex	">
+                <div className="w-full flex flex-row justify-center p-24  max-h-fit min-h-10 ">
+                    <div className="w-full max-w-desktop flex grow gap-4">
+                        <div className="flex  flex-col basis-1/2">
+                            <HeadLine text={"Mukdam Inspection system"} />
+                            <div className="text-md mt-4">
+                                PostgreSQL | Drizzle ORM | NextJS  | TypeScript |  | TailwindCSS | DaisyUI | HeadlessUI | NestJS                            </div>
+                            <div className="text-white mt-8 flex flex-col gap-6 ">
+                                <p>
+                                    Mukdam is a service that enables you to monitor and manage your pension information. The user registers the service via WhatsApp chat and subsequently receives a monthly report.                                </p>
+                                <p>
+                                    The project had 4 parts:
+                                    Writing complex queries in PostgreSQL using JSON/JSONB
+                                    Writing the algorithm that performs the checks
+                                    Building a front-end dashboard for managing the inspections (used by the account manager)
+                                    Assimilating the results from the inspection system in the front-end monthly report
+                                </p>
+                                <p>
+                                    The data received from the insurance companies arrives in XML files with a certain structure and is translated into long, complex JSON fields. The first step in this project was to extract the correct data from the correct fields in order to build the monthly report. The queries were written in PostgreSQL using JSON/JSONB. Later on, I re-wrote parts of the code using Drizzle ORM.                                </p>
+
+                            <p>The next step of this project was to perform checks for the information received from the insurance companies. Each policy’s current data is compared to the data from the last month, or to the initial data from the insurance company. The inspection system checks for deviations in the management fees, changes in the investment strategies, deviations in the expected balance, missing policies, or new policies. </p>
+                            <p>I designed the dashboard using TailwindCSS, HeadlessUI, and DaisyUI and built unique reusable components for usage across the entire app.</p>
+                            </div>
+                        </div>
+                        <div className="basis-1/2 flex flex-col gap-4 mt-8 justify-items-start">
+                            <ImageGallery imagesURLS={imagesURLS}/>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+
+        </main>
     </>
   );
 }
--- src/app/projects/pet-app/page.tsx
+++ src/app/projects/pet-app/page.tsx
@@ -1,15 +1,44 @@
-import { Section, Footer, Navbar } from "ui";
+import {Section, Footer, Navbar, HeadLine, ImageGallery} from "ui";
 
 export default function Home() {
+  const imagesURLS = [{text:"1", url:"../../../../public/svg/mailIcon.svg"}, {text:"2", url:"../../../../public/svg/githubIcon.svg"}, {text:"3", url:"../../../../public/svg/likedinIcon.svg"}]
+
   return (
     <>
       <Navbar />
 
       <main className="flex flex-col bg-body-bg-dots ">
-        <div className="bg-body-bg-shapes scroll-smooth h-screen	">
-          <Section id="aboutMe">pet-app</Section>
-          <Footer />
+        <div className="bg-body-bg-shapes scroll-smooth h-screen flex	">
+          <div className="w-full flex flex-row justify-center p-24  max-h-fit min-h-10 ">
+            <div className="w-full max-w-desktop flex grow gap-4">
+              <div className="flex  flex-col basis-1/2">
+                <HeadLine text={"Pet App"} />
+                <div className="text-md mt-4">
+                  React | MongoDB | Vercel | ChakraUI | Node.js
+                </div>
+                <div className="text-white mt-8 flex flex-col gap-6 ">
+                  <p>
+                    Get a pet today! Pawsitive adoptions allow you to get the pet you have always wished for.
+                    Search for your pet and save it to your wishlist. You can register and adopt or foster it for a short period of time.                   </p>
+                  <p>
+                    I had a great time putting this app together, I learned a lot about the meaning of being a full-stack developer and crafting the entire app end-to-end.                  </p>
+            
+                  <p>This was the final project of my studies at the ITC full-stack development bootcamp. The project had three main parts:
+                    Building the front-end using ChakraUI and additional code - guest preview, user preview, and manager preview.
+                    Building the backend database using MongoDB.
+                    Deploying the app using Vercel.
+                  </p>
+                </div>
+              </div>
+              <div className="basis-1/2 flex flex-col gap-4 mt-8 justify-items-start">
+                <ImageGallery imagesURLS={imagesURLS}/>
+
+              </div>
+            </div>
+          </div>
         </div>
+        <Footer />
+
       </main>
     </>
   );
