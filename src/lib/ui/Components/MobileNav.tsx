a1509dabe61f5a2ef5f71ef7d1a30c5de3c0e986
Zoe181991 <zoebarkan@gmail.com>
2024-03-25 19:13:53 +0200

    ..

--- src/lib/ui/Components/MobileNav.tsx
+++ src/lib/ui/Components/MobileNav.tsx
@@ -1,25 +1,25 @@
 'use client'
 
-export function MobileNav (){return(<ul className="flex justify-between  flex-col items-center w-full gap-1 ">
-    <li className="font-semibold hover:cursor-pointer hover:scale-125 transition ease-in-out duration-150">
+export function MobileNav (){return(<div className="flex  flex-col  items-center text-center w-full  ">
+    <div className="py-2 w-full text-[#f98d55] hover:bg-[#f98d55]  hover:text-base-1 font-semibold hover:cursor-pointer hover:scale-125  transition ease-in-out duration-200">
         <a href="/#aboutMe">about</a>
-    </li>
-    <li className="  hover:cursor-pointer hover:scale-125 transition ease-in-out duration-150">
+    </div>
+    <div className="py-2 w-full text-[#c974a3] hover:bg-[#c974a3]  hover:text-base-1 font-semibold text-base-4 hover:scale-125 hover:cursor-pointer  transition ease-in-out duration-300">
         <a href="https://www.linkedin.com/in/zoe-barkan-a1aa17252/"
             target="_blank">
             linkedin
         </a>
-    </li>
-    <li className="hover:cursor-pointer hover:scale-125 transition ease-in-out duration-150">
+    </div>
+    <div className="py-2 w-full font-semibold text-base-2 hover:bg-base-2  hover:text-base-1 hover:cursor-pointer hover:scale-125  transition ease-in-out duration-200">
         <a href="https://github.com/Zoe181991" target="_blank">
             github
         </a>
-    </li>
-    <li className="hover:cursor-pointer hover:scale-125 transition ease-in-out duration-150">
+    </div>
+    <div className="py-2 w-full text-base-4 hover:bg-base-4  hover:text-base-1 font-semibold hover:cursor-pointer hover:scale-125  transition ease-in-out duration-200">
         <a href="mailto:zoebarkan@gmail.com" target="_blank">
             email
         </a>
-    </li>
-</ul>
+    </div>
+</div>
 
 )}
