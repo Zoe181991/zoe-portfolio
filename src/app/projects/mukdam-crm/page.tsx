a83fbfcadbc456bc2ad2c81cd72d76e9b4d64b10
Zoe181991 <zoebarkan@gmail.com>
2024-03-27 15:09:14 +0200

    ..

--- src/app/projects/mukdam-crm/page.tsx
+++ src/app/projects/mukdam-crm/page.tsx
@@ -1,52 +1,78 @@
-import {Section, Footer, Navbar, HeadLine, ImageGallery, ImageGalleryMukdamInspectionContent} from "ui";
-import {ImageGalleryMukdamCrmContent} from "ui";
+import {
+  Section,
+  Footer,
+  Navbar,
+  HeadLine,
+  ImageGallery,
+  ImageGalleryMukdamInspectionContent,
+} from "ui";
+import { ImageGalleryMukdamCrmContent } from "ui";
 import React from "react";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 export default function Home() {
-
   return (
     <>
       <Navbar />
 
       <main className="flex flex-col bg-body-bg-dots ">
         <div className="bg-body-bg-shapes scroll-smooth	">
-            <div className="w-full flex flex-col justify-center pt-8 pb-24 px-12 md:px-24  max-h-fit min-h-10 mb-8 ">
-<div className="flex flex-row gap-8 items-center ">
-    <Link href="/" className="btn btn-circle text-base-3 hover:bg-white hover:bg-opacity-60 hover:text-base-1">❮</Link>
-<div>
+          <div className="w-full flex flex-col justify-center pt-8 pb-24 px-12 md:px-24  max-h-fit min-h-10 mb-8 ">
+            <div className="flex flex-row gap-8 items-center ">
+              <Link
+                href="/"
+                className="btn btn-circle text-base-3 hover:bg-white hover:bg-opacity-60 hover:text-base-1"
+              >
+                ❮
+              </Link>
+              <div>
                 <HeadLine text={"Mukdam CRM system"} />
 
                 <div className="text-sm lg:text-md mt-4 text-base-4">
-              NextJS | TypeScript | Drizzle ORM | TailwindCSS{" "}
+                  NextJS | TypeScript | Drizzle ORM | TailwindCSS{" "}
+                </div>
+              </div>
             </div>
-</div>
-</div>
-                <div className="w-full flex justify-between flex-col lg:flex-row mt-8 gap-4 lg:gap-8">
-                        <div className="h-fit flex flex-col basis-3/5 mockup-browser border border-base-300 bg-base-1">
-                            <div className="mockup-browser-toolbar">
-                                <div className="input border border-base-300">https://mukdam-demo.com</div>
-                            </div>
-                            <div className="flex justify-center  border-t border-base-300">
-                                <ImageGallery imagesURLS={ImageGalleryMukdamCrmContent} projectName={"mukdam-crm"}/>
-                            </div>
-                        </div>
+            <div className="w-full flex justify-between flex-col lg:flex-row mt-8 gap-4 lg:gap-8">
+              <div className="h-fit flex flex-col basis-3/5 mockup-browser border border-base-300 bg-base-1">
+                <div className=" mockup-browser-toolbar">
+                  <div className="hover:bg-white input border border-base-300">
+                    https://mukdam-demo.com
+                  </div>
+                </div>
+                <div className="flex justify-center  border-t border-base-300">
+                  <ImageGallery
+                    imagesURLS={ImageGalleryMukdamCrmContent}
+                    projectName={"mukdam-crm"}
+                  />
+                </div>
+              </div>
 
-                    <div className="flex flex-col basis-2/5  gap-8 text-white">
-                        <p>
-                            Mukdam is a service that enables you to monitor and manage your pension information. The user registers the service via WhatsApp chat and subsequently receives a monthly report.</p>
-                            <p>
-                                Mukdam is a service that enables you to monitor and manage your pension information. The user registers the service via WhatsApp chat and subsequently receives a monthly report .
-                            </p>
-                            <p>
-                                The goal of my project was to replace a third-party CRM system with an insourced system. The CRM system I built is used for Account management and connects to Make (an automation software) that handles the WhatsApp communication.
-                            </p>
-                            <p>
-                                The CRM system contains an interface for the account manager and has full CRUD options. The backend has endpoints for the account management and specific endpoints for querying that are used by the Make processes.
-                            </p>
-
-                    </div>
-        </div>
+              <div className="flex flex-col basis-2/5  gap-8 text-white">
+                <p>
+                  Mukdam is a service that enables you to monitor and manage
+                  your pension information. The user registers the service via
+                  WhatsApp chat and subsequently receives a monthly report.
+                </p>
+                <p>
+                  Mukdam is a service that enables you to monitor and manage
+                  your pension information. The user registers the service via
+                  WhatsApp chat and subsequently receives a monthly report .
+                </p>
+                <p>
+                  The goal of my project was to replace a third-party CRM system
+                  with an insourced system. The CRM system I built is used for
+                  Account management and connects to Make (an automation
+                  software) that handles the WhatsApp communication.
+                </p>
+                <p>
+                  The CRM system contains an interface for the account manager
+                  and has full CRUD options. The backend has endpoints for the
+                  account management and specific endpoints for querying that
+                  are used by the Make processes.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- src/lib/ui/Components/ImageGallery.tsx
+++ src/lib/ui/Components/ImageGallery.tsx
@@ -1,135 +1,160 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import Image from "next/image";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import arrowsMaximize from "../../../../public/svg/arrowsMaximize.svg";
 
 interface ImageGalleryProps {
-    imagesURLS:{text: string, src: string } [];
-    projectName: string;
+  imagesURLS: { text: string; src: string }[];
+  projectName: string;
 }
-export function ImageGallery({imagesURLS, projectName}: ImageGalleryProps) {
-    const router = useRouter()
+export function ImageGallery({ imagesURLS, projectName }: ImageGalleryProps) {
+  const router = useRouter();
 
-    const [activeIndex, setActiveIndex] = React.useState(0);
-    const [activeIndexModal, setActiveIndexModal] = React.useState(activeIndex);
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [activeIndexModal, setActiveIndexModal] = React.useState(activeIndex);
 
-    const handleNext = (index: number) => {
-        console.log("activeIndex:", activeIndex, "index:", index)
-        if(activeIndex === imagesURLS.length-1) {
-            setActiveIndex(0)
-            router.push(`/projects/${projectName}/#slide${0}`)
-
-        }else{
-            setActiveIndex((prev) => (prev + 1) );
-            router.push(`/projects/${projectName}/#slide${index+1}`)
-
-        }
+  const handleNext = (index: number) => {
+    console.log("activeIndex:", activeIndex, "index:", index);
+    if (activeIndex === imagesURLS.length - 1) {
+      setActiveIndex(0);
+      router.push(`/projects/${projectName}/#slide${0}`);
+    } else {
+      setActiveIndex((prev) => prev + 1);
+      router.push(`/projects/${projectName}/#slide${index + 1}`);
     }
+  };
 
-    const handlePrevious = (index: number) => {
-        console.log("activeIndex:", activeIndex, "index:", index)
-        if(activeIndex === 0) {
-            setActiveIndex(imagesURLS.length-1)
-            const newIndex = imagesURLS.length-1
-            router.push(`/projects/${projectName}/#slide${newIndex}`)
-
-        } else{
-            const newIndex = index-1
-            setActiveIndex((prev) => (prev - 1) );
-            router.push(`/projects/${projectName}/#slide${newIndex}`)
-
-        }
+  const handlePrevious = (index: number) => {
+    console.log("activeIndex:", activeIndex, "index:", index);
+    if (activeIndex === 0) {
+      setActiveIndex(imagesURLS.length - 1);
+      const newIndex = imagesURLS.length - 1;
+      router.push(`/projects/${projectName}/#slide${newIndex}`);
+    } else {
+      const newIndex = index - 1;
+      setActiveIndex((prev) => prev - 1);
+      router.push(`/projects/${projectName}/#slide${newIndex}`);
     }
+  };
 
-
-    const handleNextModal = () => {
-        console.log("activeIndex:", activeIndex)
-        if(activeIndexModal === imagesURLS.length-1) {
-            setActiveIndexModal(0)
-        }else{
-            setActiveIndexModal((prev) => (prev + 1) );
-        }
-
+  const handleNextModal = () => {
+    console.log("activeIndex:", activeIndex);
+    if (activeIndexModal === imagesURLS.length - 1) {
+      setActiveIndexModal(0);
+    } else {
+      setActiveIndexModal((prev) => prev + 1);
     }
-    const handlePreviousModal = () => {
-        console.log("activeIndex:", activeIndex)
-        if(activeIndexModal === 0) {
-            setActiveIndexModal(imagesURLS.length-1)
-        } else{
-            setActiveIndexModal((prev) => (prev - 1) );
-        }
-
+  };
+  const handlePreviousModal = () => {
+    console.log("activeIndex:", activeIndex);
+    if (activeIndexModal === 0) {
+      setActiveIndexModal(imagesURLS.length - 1);
+    } else {
+      setActiveIndexModal((prev) => prev - 1);
     }
+  };
 
-    return (
-        <>
-        <div id="carousel" className="carousel w-full">
-                { imagesURLS.map((image, index) => {
-                    return (
-                        <>
-                        <div key={index} id={`slide${index}`} className="carousel-item relative w-full">
-                            <img src={image.src} className="w-full" />
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a onClick={()=>handlePrevious(index)} className="btn btn-circle opacity-60">❮</a>
-                                <a onClick={()=>handleNext(index)} className="btn btn-circle opacity-60">❯</a>
-                            </div>
-                            <div className='absolute bottom-0 left-0 bg-base-2 w-full h-16  bg-opacity-80 text-base-1 p-4'>
-
-                               <div className="flex justify-between items-center">
-                                  <span className="text-white"> {image.text}</span>
-                                   <button className="btn-xs" onClick={()=>document.getElementById('my_modal_3').showModal()}><Image src={arrowsMaximize} alt={"maximize"} width={24} height={24} /></button>
-
-                               </div>
-
-                            </div>
-                        </div>
-                        </>
-                    )
-                })}
-        </div>
-
-
-
-            <dialog id="my_modal_3" className="modal">
-                <div className="modal-box w-11/12 max-w-5xl h-screen">
-                    <form method="dialog">
-                        <button className="btn btn-md btn-circle btn-ghost absolute right-2 top-2 text-lg">✕</button>
-                    </form>
-                    <div className="carousel w-full">
-
-                    { imagesURLS.map((image, indexModal) => {
-                                    return (
-                                        <>
-                                        <div key={indexModal} className={(indexModal===activeIndexModal)? " w-full opacity-100 duration-700 ease-in-out " : "hidden"}>
-                                            <h3 className="font-bold text-lg mb-2">{image.text}</h3>
-                                            <div className="carousel-item relative">
-                                            <Image  src={image.src} alt={image.text} layout="responsive" width={680} height={480} />
-                                                <div className="absolute flex opacity-60 justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                                    <a onClick={handlePreviousModal}  className="btn btn-circle">❮</a>
-                                                    <a onClick={handleNextModal} className="btn btn-circle">❯</a>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        </>
-                                    )
-                                })}
-                    </div>
-
+  return (
+    <>
+      <div id="carousel" className="carousel w-full">
+        {imagesURLS.map((image, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                id={`slide${index}`}
+                className="carousel-item relative w-full"
+              >
+                <img src={image.src} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a
+                    onClick={() => handlePrevious(index)}
+                    className="btn btn-circle opacity-60"
+                  >
+                    ❮
+                  </a>
+                  <a
+                    onClick={() => handleNext(index)}
+                    className="btn btn-circle opacity-60"
+                  >
+                    ❯
+                  </a>
                 </div>
-                <form method="dialog" className="modal-backdrop">
-                    <button>close</button>
-                </form>
-            </dialog>
+                <div className="absolute bottom-0 left-0 bg-base-2 w-full h-16  bg-opacity-80 text-base-1 p-4">
+                  <div className="flex justify-between items-center">
+                    <span className="text-white"> {image.text}</span>
+                    <button
+                      className="btn-xs"
+                      onClick={() =>
+                        document.getElementById("my_modal_3").showModal()
+                      }
+                    >
+                      <Image
+                        src={arrowsMaximize}
+                        alt={"maximize"}
+                        width={24}
+                        height={24}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
 
-
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box w-11/12 max-w-5xl h-screen">
+          <form method="dialog">
+            <button className="btn btn-md btn-circle btn-ghost absolute right-2 top-2 text-lg">
+              ✕
+            </button>
+          </form>
+          <div className="carousel w-full">
+            {imagesURLS.map((image, indexModal) => {
+              return (
+                <>
+                  <div
+                    key={indexModal}
+                    className={
+                      indexModal === activeIndexModal
+                        ? " w-full opacity-100 duration-700 ease-in-out "
+                        : "hidden"
+                    }
+                  >
+                    <h3 className="font-bold text-lg mb-2">{image.text}</h3>
+                    <div className="carousel-item relative">
+                      <Image
+                        src={image.src}
+                        alt={image.text}
+                        layout="responsive"
+                        width={680}
+                        height={480}
+                      />
+                      <div className="absolute flex opacity-60 justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a
+                          onClick={handlePreviousModal}
+                          className="btn btn-circle"
+                        >
+                          ❮
+                        </a>
+                        <a onClick={handleNextModal} className="btn btn-circle">
+                          ❯
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </>
-
-
-
-    );
+  );
 }
-
-
-
