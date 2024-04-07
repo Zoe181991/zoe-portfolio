f42f3aebf35b6fa12816d3e0c12362c42ab8ffb6
Zoe181991 <zoebarkan@gmail.com>
2024-04-07 11:32:48 +0300

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? "/zoe-portfolio" : "";
 const nextConfig = {
   basePath: basePath,
+  assetPrefix: basePath,
   output: "export",
   reactStrictMode: true,
   distDir: "dist",
